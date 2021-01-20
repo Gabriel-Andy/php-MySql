db86d7b94debf170add8b2451256fa7df1e4a66a
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-20 05:10:44 -0500

    I added php script

---
+++ report.php
@@ -0,0 +1,14 @@
+<?php
+$when_it_happened = $_POST["whenithappend"]
+$how_long = $_POST["howlong"]
+$alien_description = $_POST["description]
+$fang_spotted = $_POST ['fangspotted']
+$email = $_POST['email]
+
+echo 'thanks for submitting the form.<br/>';
+echo' youu were abducted ' .$when_it_happened;
+echo'and were gone for' .$how_long. '<br/>';
+echo 'Describe them '. $alien_description . '<br/>';
+echo 'was Fang there?'. $fang_spotted. ' <br/>';
+echo 'was email address is' . $email;
+?>
