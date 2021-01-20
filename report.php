64b8c812b0d3e9985ea32f55b8661445c00592bd
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-20 09:33:15 -0500

    i added few logic

--- report.php
+++ report.php
@@ -1,14 +1,26 @@
-<?php
-$when_it_happened = $_POST["whenithappend"]
-$how_long = $_POST["howlong"]
-$alien_description = $_POST["description]
-$fang_spotted = $_POST ['fangspotted']
-$email = $_POST['email]
 
-echo 'thanks for submitting the form.<br/>';
-echo' youu were abducted ' .$when_it_happened;
-echo'and were gone for' .$how_long. '<br/>';
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Aliens Abducted me - Report an Abducted</title>
+ </head>
+ <body>
+     <h2>Aliens Abducted me - Report an Abducted</h2>
+      <?php
+$when_it_happened = $_POST["whenithappend"];
+$how_long = $_POST["howlong"];
+$alien_description = $_POST["description"];
+$fang_spotted = $_POST['fangspotted'];
+$email = $_POST['email'];
+
+echo 'thanks for submitting the form. <br/>';
+echo' you were abducted ' .$when_it_happened;
+echo'and were gone for' . $how_long. '<br/>';
 echo 'Describe them '. $alien_description . '<br/>';
-echo 'was Fang there?'. $fang_spotted. ' <br/>';
+echo 'was Fang there?'. $fang_spotted. '<br/>';
 echo 'was email address is' . $email;
 ?>
+ </body>
+ </html>
